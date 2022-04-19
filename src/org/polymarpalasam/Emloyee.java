bc5c45e5bda75b426ba3233d50d12148c1d5bef4
EniyavanThangaraj <eniyavant405@gmail.com>
2022-04-19 15:22:17 +0530

    project strucutre

---
+++ src/org/polymarpalasam/Emloyee.java
@@ -0,0 +1,74 @@
+package org.polymarpalasam;
+
+public class Emloyee {
+
+private void empaloyeeDettails(String name) {
+System.out.println("emloyee id is "+name);
+}	
+	
+	private void empDetaills(int id) {
+		System.out.println("employee id is"+id);
+
+	}
+	
+	private void empDetails(String name) {
+		System.out.println("employee name is "+name);
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		Emloyee em=new Emloyee();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
